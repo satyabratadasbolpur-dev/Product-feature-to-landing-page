ee412a069ad94681feddfb43379a46b2f51387c1
satyabratadasbolpur-dev <satyabratadas.bolpur@gmail.com>
2026-04-05 10:46:11 +0530

    Add files via upload

---
+++ Feature.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import google.generativeai as genai
+genai.configure(api_key=st.secrects["API_KEY"])
+nontu_model = genai.GenerativeModel("gemini-2.5-flash")
+st.title(" Feature List to Landing Page Copy")
+product = st.text_input(" List of product features")
+if st.button("Submit"):
+    if product:
+        prompt = f"Create persuasive landing page content for a {product} with these features"
+        response = nontu_model.generate_content(prompt)
+        st.markdown(response.text) 
+else:
+    st.warning("Create persuasive")
+
+    
